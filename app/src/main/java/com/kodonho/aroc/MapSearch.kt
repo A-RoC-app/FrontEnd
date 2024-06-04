d52b49e916260a0a46f30c0e6dd9b889f9736691
jiyounghi <wldud020101@naver.com>
2024-06-04 23:06:31 +0900

    UserListView Upload

---
+++ app/src/main/java/com/kodonho/aroc/MapSearch.kt
@@ -0,0 +1,20 @@
+package com.kodonho.aroc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MapSearch : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_map_search)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.map_search)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/kodonho/aroc/UserListView.kt
@@ -0,0 +1,20 @@
+package com.kodonho.aroc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UserListView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_user_list_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.user_list_view)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
